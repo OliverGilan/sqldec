b091a28d9e11506643e15034cc30cf6916b44d66
Oliver Gilan <github@olivergilan.com>
2022-08-11 11:23:16 -0400

    default schema file. command steps broken down. bugs cleaned up with arg parsing

---
+++ default_schema.sql
@@ -0,0 +1,11 @@
+create schema if not exists public;
+
+create schema if not exists extensions;
+create extension if not exists "uuid-ossp"      with schema extensions;
+
+create table if not exists public.psqldec (
+  v serial primary key,
+  current_migration not null text,
+  changed_on datetime not null default now()
+);
+
