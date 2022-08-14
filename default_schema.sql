452fa7e72128134c22a1c8d6cea5901ff2966a98
Oliver Gilan <github@olivergilan.com>
2022-08-14 00:51:22 -0400

    apply wip

--- default_schema.sql
+++ default_schema.sql
@@ -1,11 +1,11 @@
-create schema if not exists public;
+create schema if not exists psqldec;
 
 create schema if not exists extensions;
-create extension if not exists "uuid-ossp"      with schema extensions;
+create extension if not exists "uuid-ossp" with schema extensions;
 
-create table if not exists public.psqldec (
+create table if not exists psqldec.version (
   v serial primary key,
-  current_migration not null text,
+  current_migration text not null,
   changed_on timestamp not null default now()
 );
 
--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
-create schema if not exists gooch;
+create schema if not exists psqldec;
 
 create schema if not exists extensions;
 create extension if not exists "uuid-ossp" with schema extensions;
 
-create table if not exists gooch.psqldec (
+create table if not exists psqldec.version (
   v serial primary key,
   current_migration text not null,
   changed_on timestamp not null default now()
