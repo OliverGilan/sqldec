57a7e1820e19d4959a633a437390a522a3048e46
Oliver Gilan <github@olivergilan.com>
2022-08-14 21:38:56 -0400

    apply

--- schema.sql
+++ schema.sql
@@ -1,11 +1,12 @@
-create schema if not exists psqldec;
 
 create schema if not exists extensions;
 create extension if not exists "uuid-ossp" with schema extensions;
 
-create table if not exists psqldec.version (
+-- Needed for SQLDEC // DO NOT DELETE
+create schema if not exists sqldec;
+create table if not exists sqldec.version (
   v serial primary key,
   current_migration text not null,
   changed_on timestamp not null default now()
 );
-
+-- 
